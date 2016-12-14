6e7a06dcd470fc7d43f79d1c13e1fd78da37e4ea
LaxWasHere <LaxWasHere@PoweredByHate.net>
2016-12-13 20:38:12 -0800

    Add 'more than one hub'

--- src/main/java/net/lazlecraft/hubkick/HubKick.java
+++ src/main/java/net/lazlecraft/hubkick/HubKick.java
@@ -14,6 +14,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class HubKick extends JavaPlugin implements Listener {
 	
@@ -93,7 +94,7 @@ public class HubKick extends JavaPlugin implements Listener {
 		    p.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("toLobbyMsg")));
 		    ByteArrayDataOutput out = ByteStreams.newDataOutput();
 		      out.writeUTF("Connect");
-		      out.writeUTF(getConfig().getString("HubServer"));
+		      out.writeUTF(getHubServer());
 		    p.sendPluginMessage(this, "BungeeCord", out.toByteArray());
    }
 	  	  
@@ -103,7 +104,7 @@ public class HubKick extends JavaPlugin implements Listener {
 	    	   p.sendMessage(ChatColor.translateAlternateColorCodes('&', kickallMessage));
 	    	   ByteArrayDataOutput out = ByteStreams.newDataOutput();
 			      out.writeUTF("Connect");
-			      out.writeUTF(getConfig().getString("HubServer"));
+			      out.writeUTF(getHubServer());
 			    p.sendPluginMessage(this, "BungeeCord", out.toByteArray());
 	       }
 	  }
@@ -128,5 +129,14 @@ public class HubKick extends JavaPlugin implements Listener {
 				  ev.setCancelled(true);
 				  sendPlayer(p);
 		} 
+	}
+
+	public String getHubServer() {
+		if (getConfig().getBoolean("randomHubs")) {
+			Random r = new Random();
+			return getConfig().getStringList("Hubs").get(r.nextInt(getConfig().getStringList("Hubs").size()));
+		} else {
+			return getConfig().getString("HubServer");
+		}
 	}
 }
